e163e10897f9f7679998deeba92b76a97f12e3af
Rahul Kumar Mehta <rk6658542@gmail.com>
2024-11-15 23:38:27 +0530

    Initial commit

--- components/ui/TrendingMovies.jsx
+++ components/ui/TrendingMovies.jsx
@@ -1,31 +1,76 @@
-import { View, Text } from 'react-native';
-import React from 'react';
-import Carousel from "react-native-snap-carousel";
+import { View, Text, useWindowDimensions, TouchableWithoutFeedback, Image, Animated, FlatList } from 'react-native';
+import React, { useRef, useState } from 'react';
+
 
 const TrendingMovies = ({data}) => {
+
+    let {width, height} = useWindowDimensions();
+    const ITEM_SIZE = width*0.6;
+    const scrollX = useRef(new Animated.Value(0)).current;
+
   return (
     <View className='mb-8' >
       <Text
       className='text-white text-xl mb-5'
       >TrendingMovies</Text>
 
-      <Carousel 
-      data = {data} 
-      renderItem = {({item}) => <MovieCard item = {item} /> }
-      firstItem= {1}
-      inactiveSlideOpacity = {0.60}
-      sliderWidth={600}
-      itemWidth={400}
-      slideStyle={{display:"flex", alignItems:"center"}}
+      <Animated.FlatList 
+      showsHorizontalScrollIndicator = {false}
+      data = {data}
+      keyExtractor={(key) => data.key }
+      horizontal
+      contentContainerStyle = {{
+        alignItems:'center'
+      }}
+      scrollEventThrottle={16}
+      snapToInterval={ITEM_SIZE}
+      decelerationRate={0}
+      renderItem={({item, index }) => <MovieCard key={index} scrollX = {scrollX} item={item} index = {index} /> }
+      bounces={false}
+      onScroll={Animated.event([{ nativeEvent : { contentOffset: { x: scrollX } } }], 
+                { useNativeDriver: true }
+
+      )}
       />
+
     </View>
   );
 };
 
-const MovieCard = ({item}) => {
+const MovieCard = ({item, scrollX ,index}) => {
+
+    let {width, height} = useWindowDimensions();
+    const ITEM_SIZE = width*0.6;
+    const inputRange = [
+        (index - 1) * ITEM_SIZE,
+        index * ITEM_SIZE,
+        (index + 1 ) * ITEM_SIZE
+    ]
+
+    const scale = scrollX.interpolate({
+        inputRange,
+        outputRange: [0.9, 1, 0.9]
+    })
 
     return(
-       <Text>Heelo</Text>
+       
+        <TouchableWithoutFeedback>
+            <Animated.View
+            style = {{transform: [ {scale}]}}
+            >
+
+            <Image 
+            source={require("../../assets/images/bggs.jpg")}
+            style = {{
+                width: width*0.7,
+                height: height*0.5,
+
+            }}
+            className='rounded-3xl mx-2 '
+            />
+            </Animated.View>
+
+        </TouchableWithoutFeedback>
     );
 };
 
